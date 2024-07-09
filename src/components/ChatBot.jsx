89c10f2262bce7e4e389d81d6bf930711041e0a9
MAURO0189 <108503288+MAURO0189@users.noreply.github.com>
2024-07-08 19:10:08 -0500

    add new component

---
+++ src/components/ChatBot.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function ChatBot() {
+  return (
+    <>
+      <button className="botonChat">Chatea con Markbot</button>
+      <div className="chatBot">
+        <div className="p-4">
+          <h5>Markbot</h5>
+          <p>Bienvenido, soy Markbot...</p>
+          <p>¿Qué te gustaría saber?</p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default ChatBot;
