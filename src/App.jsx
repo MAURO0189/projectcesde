28345087b8e721610ba9ecd070ebea726beb5287
MAURO0189 <108503288+MAURO0189@users.noreply.github.com>
2024-07-13 11:52:12 -0500

    add new route and add context

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,28 @@ import Movilidad from "./components/Movilidad";
 import SaludPage from "./components/SaludPage";
 import About from "./components/About";
 import SingUp from "./components/SingUp";
+import { UserProvider } from "./context/UserContext";
+import NotBackRoute from "./components/routes/NotBackRoute";
+import ProtectedRoute from "./components/routes/ProtectedRoute";
 
 function App() {
   return (
     <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<SingUp />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/movilidad" element={<Movilidad />} />
-          <Route path="/saludPage" element={<SaludPage />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </Router>
+      <UserProvider>
+        <Router>
+          <Routes>
+            <Route element={<NotBackRoute />}>
+              <Route path="/" element={<SingUp />} />
+            </Route>
+            <Route element={<ProtectedRoute />}>
+              <Route path="/home" element={<Home />} />
+            </Route>
+            <Route path="/movilidad" element={<Movilidad />} />
+            <Route path="/saludPage" element={<SaludPage />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </Router>
+      </UserProvider>
     </>
   );
 }
