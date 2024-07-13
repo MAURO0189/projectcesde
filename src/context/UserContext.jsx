a1d5b67d4ec3f1a5fdb7143add9d58fb9084785d
MAURO0189 <108503288+MAURO0189@users.noreply.github.com>
2024-07-13 11:51:13 -0500

    create context

---
+++ src/context/UserContext.jsx
@@ -0,0 +1,19 @@
+import React, { createContext, useContext, useState } from "react";
+
+const UserContext = createContext();
+
+export const useUser = () => useContext(UserContext);
+
+export const UserProvider = ({ children }) => {
+  const [user, setUser] = useState(() => {
+    const storedUser = localStorage.getItem("user");
+    return storedUser ? JSON.parse(storedUser) : null;
+  });
+  const [token, setToken] = useState(localStorage.getItem("userToken") || null);
+
+  return (
+    <UserContext.Provider value={{ user, setUser, token, setToken }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
